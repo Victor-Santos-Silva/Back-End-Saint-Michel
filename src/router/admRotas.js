5643ff1c0ae0ec56a9130bba4023536f1aab4390
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-26 16:26:36 -0300

    colocando authenticate nas rotas

--- src/router/admRotas.js
+++ src/router/admRotas.js
@@ -1,13 +1,8 @@
 const { Router } = require("express");
 const admController = require("../controller/admController");
 const { validateAdmin, validadeAdminId } = require("../middlewares/validateAdmin")
+const authenticateToken = require("../middlewares/authenticateToken.js");
 const router = Router();
-
-//busca todos os admins
-router.get('/', admController.getAll);
-
-// funcao buscar unico
-router.get('/:id', validadeAdminId, admController.getOne);
 
 //Login ADM
 router.post("/login", admController.login);
@@ -15,14 +10,21 @@ router.post("/login", admController.login);
 //Cadastrar ADM
 router.post("/cadastro", validateAdmin, admController.create);
 
+
+//busca todos os admins
+router.get('/', authenticateToken, admController.getAll);
+
+// funcao buscar unico
+router.get('/:id', authenticateToken, validadeAdminId, admController.getOne);
+
 // funcao de editar ADM
-router.put('/:id', validateAdmin, validadeAdminId, admController.update);
+router.put('/:id', authenticateToken, validateAdmin, validadeAdminId, admController.update);
 
 //funçao de esqueci senha
-router.put('/:id', validateAdmin, validadeAdminId, admController.esqueciSenha)
+router.put('/:id', authenticateToken, validateAdmin, validadeAdminId, admController.esqueciSenha)
 
 // funcao de deletar
-router.delete('/:id', validadeAdminId, admController.delete);
+router.delete('/:id', authenticateToken, validadeAdminId, admController.delete);
 
 
 module.exports = router;

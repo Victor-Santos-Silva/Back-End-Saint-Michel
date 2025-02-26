b51a5897afa6c8b29a792b20c1fb2efae384ef20
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-26 15:46:01 -0300

    adicionando crud no adm

--- src/router/admRotas.js
+++ src/router/admRotas.js
@@ -3,7 +3,26 @@ const admController = require("../controller/admController");
 const { validateAdmin, validadeAdminId } = require("../middlewares/validateAdmin")
 const router = Router();
 
+//busca todos os admins
+router.get('/', admController.getAll);
+
+// funcao buscar unico
+router.get('/:id', validadeAdminId, admController.getOne);
+
+//Login ADM
 router.post("/login", admController.login);
+
+//Cadastrar ADM
 router.post("/cadastro", validateAdmin, admController.create);
+
+// funcao de editar ADM
+router.put('/:id', validateAdmin, validadeAdminId, admController.update);
+
+//funçao de esqueci senha
+router.put('/:id', validateAdmin, validadeAdminId, admController.esqueciSenha)
+
+// funcao de deletar
+router.delete('/:id', validadeAdminId, admController.delete);
+
 
 module.exports = router;
--- src/services/admService.js
+++ src/services/admService.js
@@ -15,6 +15,67 @@ const admService = {
             console.error('Erro ao criar Adm:', error);
             throw error;
         }
+    },
+    esqueciSenha: async (email, novaSenha) => {
+        try {
+            const admin = await Adm.findByPk(id);
+            if (!admin) {
+                return null;
+            }
+
+            if (admin.email !== email) {
+                return null;
+            }
+
+            const hashSenha = await bcrypt.hash(novaSenha, 10);
+            await admin.update({ senha: hashSenha });
+            return admin;
+        } catch (error) {
+            throw new Error("Ocorreu um erro ao trocar a senha do Admin");
+        }
+    },
+    update: async (id, adminToUpdate) => {
+        try {
+            const admin = await Adm.findByPk(id);
+            if (!admin) { // user for vazio
+                return null;
+            }
+            await admin.update(adminToUpdate);
+            await admin.save();
+            return admin;
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao atualizar admin');
+        }
+    },
+    getById: async (id) => {
+        try {
+            const admin = await Adm.findByPk(id);
+            if (!admin) {
+                return null;
+            }
+            return admin;
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao buscar um unico admin');
+        }
+    },
+    getAll: async () => {
+        try {
+            return await Adm.findAll();
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao buscar todos os admins');
+        }
+    },
+    delete: async (id) => {
+        try {
+            const user = await Adm.findByPk(id);
+            if (!user) {
+                return null;
+            }
+            await user.destroy();
+            return user;
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao deletar o admin');
+        }
     }
 };
 

ed675acb1bcfb4cfce08a10265bcbf0dd7739d2b
Victor dos Santos <victorsantos.goal@gmail.com>
2025-03-03 15:58:43 -0300

    mudando models medico

--- src/models/medico.js
+++ src/models/medico.js
@@ -5,22 +5,37 @@ const Medico = sequelize.define("Medico", {
     nome_completo: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+            notEmpty: { msg: "O nome completo não pode estar vazio." },
+        },
     },
     idade: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        validate: {
+            min: { args: [18], msg: "A idade deve ser maior ou igual a 18 anos." },
+        },
     },
     cpf: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
+        validate: {
+            len: { args: [11, 11], msg: "O CPF deve ter 11 dígitos." },
+            isNumeric: { msg: "O CPF deve conter apenas números." },
+        },
     },
     crm: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
     },
     telefone: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+            len: { args: [10, 11], msg: "O telefone deve ter 10 ou 11 dígitos." },
+        },
     },
     endereco: {
         type: DataTypes.STRING,
@@ -37,17 +52,22 @@ const Medico = sequelize.define("Medico", {
     email_corporativo: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: { msg: "Informe um email válido." },
+        },
     },
     senha_corporativa: {
         type: DataTypes.STRING,
         allowNull: false,
-    }, 
+    },
     foto: {
         type: DataTypes.STRING, // Apenas o caminho do arquivo
-        allowNull: true
-    }
-
-    
+        allowNull: true,
+    },
+}, {
+    timestamps: true, // Adiciona createdAt e updatedAt automaticamente
+    tableName: "medicos", // Nome explícito da tabela
 });
 
-module.exports = Medico
+module.exports = Medico;

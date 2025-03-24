2cb5c989d91d39962ce1ca4c5ba8c58373e5ab78
Victor dos Santos <victorsantos.goal@gmail.com>
2025-03-24 08:24:14 -0300

    Colocando rota para pegar as imagens de uploads

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./router/router");
+const path = require("path");
 const { sequelize } = require("./models");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/", routes);
 

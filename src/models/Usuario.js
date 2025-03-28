4ecf52d38077eecc78feb9d1cb8033a4cae69ff6
Victor dos Santos <victorsantos.goal@gmail.com>
2025-03-27 21:25:27 -0300

    updat

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 
-const Usuario = sequelize.define('Usuario', {
+const Usuario = sequelize.define('usuarios', {
     nomeCompleto: {
         type: DataTypes.STRING,
         allowNull: false,

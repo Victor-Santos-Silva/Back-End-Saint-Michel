b786358470319c62083f7a4857d7a213fe28f9d9
IgorNakajima <nakajimaigor@gmail.com>
2025-05-16 14:55:08 -0300

    optionsSuccessStatus: 200

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    optionsSuccessStatus: 200
+    
 }));
 
 

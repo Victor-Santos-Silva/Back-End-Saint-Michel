adbe0cd19a621eddb93604173e2a8217480f8658
IgorNakajima <nakajimaigor@gmail.com>
2025-05-16 14:57:36 -0300

    requisição

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: '*',
-    
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     optionsSuccessStatus: 200

6299a30499e43a9e84b0ef7bb385712c54f276f1
IgorNakajima <nakajimaigor@gmail.com>
2025-05-16 15:31:32 -0300

    atualizações4

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./router/router");
-
+const path = require("path");
 const { sequelize } = require("./models");
 const app = express();
 

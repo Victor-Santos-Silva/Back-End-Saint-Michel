21acbe61c769dfd75dfa76c8878ce4e6e29f3a6b
IgorNakajima <nakajimaigor@gmail.com>
2025-05-16 14:50:00 -0300

    credentials: true,

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
     optionsSuccessStatus: 200
 }));
 

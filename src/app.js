d7e45d136677f2250214530e6402f1d94c063c32
IgorNakajima <nakajimaigor@gmail.com>
2025-05-16 14:57:55 -0300

    allowedHeaders: ['Content-Type', 'Authorization'],

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    
     credentials: true,
     optionsSuccessStatus: 200
 }));

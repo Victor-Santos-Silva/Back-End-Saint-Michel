4cd27968dd6bc56117e5f66f28cfb3acfe14df97
Victor dos Santos <victorsantos.goal@gmail.com>
2025-06-03 00:50:33 -0300

    voltando database

--- src/config/database.js
+++ src/config/database.js
@@ -7,13 +7,14 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'mssql',
+        dialect: 'mysql',
+       /*  dialect: 'mssql',
         dialectOptions: {
             options: {
                 encrypt: true, // Obrigatório para Azure SQL
                 trustServerCertificate: false, // Correto para produção
             }
-        }
+        } */
     }
 );
 module.exports = sequelize;

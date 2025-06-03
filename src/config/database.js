a5651029d62d56f932b96cab91e47fa6d7a1c902
IgorNakajima <73953861+eu-afk@users.noreply.github.com>
2025-06-03 14:23:56 -0300

    voltando pra cloud azure

--- src/config/database.js
+++ src/config/database.js
@@ -7,14 +7,14 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'mysql',
-       /*  dialect: 'mssql',
+        /* dialect: 'mysql', */
+        dialect: 'mssql',
         dialectOptions: {
             options: {
                 encrypt: true, // Obrigatório para Azure SQL
                 trustServerCertificate: false, // Correto para produção
             }
-        } */
+        }
     }
 );
 module.exports = sequelize;

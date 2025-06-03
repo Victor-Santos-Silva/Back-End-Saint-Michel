27b9ea4283d93bdcb89277b5e26ba1ce8b48cba8
Victor dos Santos <victorsantos.goal@gmail.com>
2025-06-02 23:01:00 -0300

    arrumando database

--- src/config/database.js
+++ src/config/database.js
@@ -12,32 +12,8 @@ const sequelize = new Sequelize(
             options: {
                 encrypt: true, // Obrigatório para Azure SQL
                 trustServerCertificate: false, // Correto para produção
-                requestTimeout: 30000, // Timeout de 30 segundos
-                connectTimeout: 30000 // Timeout de conexão
             }
-        },
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-        retry: {
-            max: 5, // Tentar reconectar até 5 vezes
-            match: [
-                'ECONNRESET',
-                'ETIMEDOUT',
-                'ESOCKETTIMEDOUT',
-                'EHOSTUNREACH'
-            ]
-        },
-        logging: console.log // Adicione para ver logs detalhados (remova em produção)
+        }
     }
 );
-
-// Testar a conexão
-sequelize.authenticate()
-    .then(() => console.log('Conexão com o Azure SQL estabelecida com sucesso!'))
-    .catch(err => console.error('Erro ao conectar ao Azure SQL:', err));
-
 module.exports = sequelize;

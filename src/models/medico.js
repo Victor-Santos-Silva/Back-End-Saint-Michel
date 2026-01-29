17672f11290dff8f5b9c830885d6b0d2953ed4dc
Victor dos Santos da Silva <victorsantos.goal@gmail.com>
2026-01-29 15:37:32 -0300

    reestruturando table medicos

--- src/models/medico.js
+++ src/models/medico.js
@@ -1,102 +1,67 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-const Medico = sequelize.define("Medico", {
-    nome_completo: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: { msg: "O nome completo não pode estar vazio." },
-        },
+const Medico = sequelize.define(
+  "Medico",
+  {
+    nomeCompleto: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     dataNascimento: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-        validate: {
-            isDate: { msg: "Data de nascimento inválida." },
-            isValidDate(value) {
-                const data = new Date(value);
-                const hoje = new Date();
-                const idade = hoje.getFullYear() - data.getFullYear();
-
-                if (data > hoje) throw new Error("A data de nascimento não pode ser no futuro.");
-                if (idade < 21) throw new Error("O médico deve ter no mínimo 21 anos.");
-            }
-        }
+      type: DataTypes.DATEONLY,
+      allowNull: false,
     },
     cpf: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            len: { args: [11, 11], msg: "O CPF deve ter 11 dígitos." },
-            isNumeric: { msg: "O CPF deve conter apenas números." },
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     crm: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            // Exemplo de regex para CRM: '123456/SP'
-            is: { args: /^[0-9]{6}\/[A-Z]{2}$/, msg: 'CRM inválido! Formato correto: 123456/SP' }
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     telefone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: { args: [10, 11], msg: "O telefone deve ter 10 ou 11 dígitos." },
-            isNumeric: { msg: "O telefone deve conter apenas números." },
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    endereco: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: { msg: "O endereço não pode estar vazio." }
-        }
-    },
+
     especialidade: {
-        type: DataTypes.ENUM(
-            'Ortopedista',
-            'Proctologista',
-            'Oncologista',
-            'Otorrinolaringologista',
-            'Oftalmologista',
-            'Cardiologista',
-            'Pneumologista',
-            'Nefrologista',
-            'Gastroenterologista',
-            'Urologista',
-            'Dermatologista',
-            'Ginecologista',
-        ),
-        allowNull: false,
-        validate: {
-            notEmpty: { msg: "A especialidade não pode estar vazio." }
-        }
+      type: DataTypes.ENUM(
+        "Ortopedista",
+        "Proctologista",
+        "Oncologista",
+        "Otorrinolaringologista",
+        "Oftalmologista",
+        "Cardiologista",
+        "Pneumologista",
+        "Nefrologista",
+        "Gastroenterologista",
+        "Urologista",
+        "Dermatologista",
+        "Ginecologista",
+      ),
+      allowNull: false,
     },
-    email_corporativo: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: { msg: "Informe um email válido." },
-        },
+    emailCorporativo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
-    senha_corporativa: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        len: { args: [6, 100], msg: "A senha deve ter no mínimo 6 caracteres." }
+    senhaCorporativa: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     foto: {
-        type: DataTypes.STRING, // Apenas o caminho do arquivo
-        allowNull: true,
+      type: DataTypes.STRING,
+      allowNull: true,
     },
-}, {
+  },
+  {
     timestamps: true, // Adiciona createdAt e updatedAt automaticamente
     tableName: "medicos", // Nome explícito da tabela
-});
+  },
+);
 
 module.exports = Medico;

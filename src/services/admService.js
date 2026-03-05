3a3911deacd465a17549367a895f7a2c5205c134
Victor dos Santos da Silva <victorsantos.goal@gmail.com>
2026-03-05 10:26:59 -0300

    tirando os try/catch nos services de paciente e adm

--- src/services/admService.js
+++ src/services/admService.js
@@ -36,114 +36,90 @@ const admService = {
   },
 
   esqueciSenha: async (email, novaSenha) => {
-    try {
-      // Primeiro busca o admin pelo email
-      const admin = await Administrador.findOne({ where: { email } });
+    // Primeiro busca o admin pelo email
+    const admin = await Administrador.findOne({ where: { email } });
 
-      if (!admin) {
-        throw new Error("Adm não encontrado");
-      }
-
-      // Verifica se a nova senha tem pelo menos 6 caracteres
-      if (novaSenha.length < 6) {
-        throw new Error("A senha deve ter pelo menos 6 caracteres");
-      }
-
-      const hashSenha = await bcrypt.hash(novaSenha, 10);
-      await admin.update({ senha: hashSenha });
-      return admin;
-    } catch (error) {
-      throw error;
+    if (!admin) {
+      throw new Error("Adm não encontrado");
     }
+
+    // Verifica se a nova senha tem pelo menos 6 caracteres
+    if (novaSenha.length < 6) {
+      throw new Error("A senha deve ter pelo menos 6 caracteres");
+    }
+
+    const hashSenha = await bcrypt.hash(novaSenha, 10);
+    await admin.update({ senha: hashSenha });
+    return admin;
   },
 
   // CRUD
   create: async (cadastro) => {
-    try {
-      const { nome, email, senha } = cadastro;
+    const { nome, email, senha } = cadastro;
 
-      const hashSenha = await bcrypt.hash(senha, 10);
+    const hashSenha = await bcrypt.hash(senha, 10);
 
-      const adminExistente = await Administrador.findOne({ where: { email } });
-      if (adminExistente) {
-        throw new Error("Email já cadastrado.");
-      }
-
-      if (!nome || !email || !senha) {
-        throw new Error("Todos os campos são obrigatórios.");
-      }
-
-      return await Administrador.create({
-        nome,
-        email,
-        senha: hashSenha,
-      });
-    } catch (error) {
-      throw error;
+    const adminExistente = await Administrador.findOne({ where: { email } });
+    if (adminExistente) {
+      throw new Error("Email já cadastrado.");
     }
+
+    if (!nome || !email || !senha) {
+      throw new Error("Todos os campos são obrigatórios.");
+    }
+
+    return await Administrador.create({
+      nome,
+      email,
+      senha: hashSenha,
+    });
   },
 
   update: async (id, adminToUpdate) => {
-    try {
-      const admin = await Administrador.findByPk(id);
-      if (!admin) {
-        throw new Error("Adm não encontrado.");
-      }
-
-      if (adminToUpdate.email) {
-        const emailExistente = await Administrador.findOne({
-          where: { email: adminToUpdate.email },
-        });
-        if (emailExistente && emailExistente.id !== id) {
-          throw new Error("Email já cadastrado por outro admin.");
-        }
-      }
-
-      if (adminToUpdate.senha) {
-        const hashSenha = await bcrypt.hash(adminToUpdate.senha, 10);
-        adminToUpdate.senha = hashSenha;
-      }
-
-      await admin.update(adminToUpdate);
-      return admin;
-    } catch (error) {
-      throw error;
+    const admin = await Administrador.findByPk(id);
+    if (!admin) {
+      throw new Error("Adm não encontrado.");
     }
+
+    if (adminToUpdate.email) {
+      const emailExistente = await Administrador.findOne({
+        where: { email: adminToUpdate.email },
+      });
+      if (emailExistente && emailExistente.id !== id) {
+        throw new Error("Email já cadastrado por outro admin.");
+      }
+    }
+
+    if (adminToUpdate.senha) {
+      const hashSenha = await bcrypt.hash(adminToUpdate.senha, 10);
+      adminToUpdate.senha = hashSenha;
+    }
+
+    await admin.update(adminToUpdate);
+    return admin;
   },
   getById: async (id) => {
-    try {
-      const admin = await Administrador.findByPk(id, {
-        attributes: { exclude: ["senha"] },
-      });
-      if (!admin) {
-        throw new Error("Administrador não encontrado.");
-      }
-
-      return admin;
-    } catch (error) {
-      throw error;
+    const admin = await Administrador.findByPk(id, {
+      attributes: { exclude: ["senha"] },
+    });
+    if (!admin) {
+      throw new Error("Administrador não encontrado.");
     }
+
+    return admin;
   },
   getAll: async () => {
-    try {
-      return await Administrador.findAll({
-        attributes: { exclude: ["senha"] },
-      });
-    } catch (error) {
-      throw new Error("Ocorreu um erro ao buscar todos os admins");
-    }
+    return await Administrador.findAll({
+      attributes: { exclude: ["senha"] },
+    });
   },
   delete: async (id) => {
-    try {
-      const user = await Administrador.findByPk(id);
-      if (!user) {
-        throw new Error("Administrador não encontrado.");
-      }
-      await user.destroy();
-      return user;
-    } catch (error) {
-      throw error;
+    const user = await Administrador.findByPk(id);
+    if (!user) {
+      throw new Error("Administrador não encontrado.");
     }
+    await user.destroy();
+    return user;
   },
 };
 
